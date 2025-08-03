95249a5808187933b983242dfd34fff9996a8561
“Zuzana <zuzana.moskvic@gmail.com>
2025-08-03 21:08:22 +0200

    Clean up

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -114,14 +114,7 @@ router.post("/auth/login", (req, res, next) => {
   });
 
 // GET  /auth/verify  -  Used to verify JWT stored on the client
-router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
- 
-  // If JWT token is valid the payload gets decoded by the
-  // isAuthenticated middleware and made available on `req.payload`
-  console.log(`req.payload`, req.payload);
- 
-  // Send back the object with user data
-  // previously set as the token payload
+router.get('/verify', isAuthenticated, (req, res, next) => {       
   res.status(200).json(req.payload);
 });
 
