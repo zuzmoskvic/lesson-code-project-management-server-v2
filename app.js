f22b8aada3e61bfb983751d63694fe6d918cf23f
“Zuzana <zuzana.moskvic@gmail.com>
2025-08-03 20:59:47 +0200

    Adjusted

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv/config");
 require("./db");
 const express = require("express");
 
+const { isAuthenticated } = require("./middleware/jwt.middleware");
+
 const app = express();
 
 require("./config")(app);
@@ -11,10 +13,10 @@ const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 const projectRouter = require("./routes/project.routes");
-app.use("/api", projectRouter);
+app.use("/api", isAuthenticated, projectRouter);
 
 const taskRouter = require("./routes/task.routes");
-app.use("/api", taskRouter);
+app.use("/api", isAuthenticated, taskRouter);
 
 const authRouter = require("./routes/auth.routes");
 app.use("/api", authRouter);
